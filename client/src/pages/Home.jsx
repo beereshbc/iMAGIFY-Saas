3d9c368cdafe63392a458049583eef05873c1277
beereshbc <bcbeereshkumar@gmail.com>
2025-02-11 18:38:30 +0530

    Header file correction

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "../components/hEADER.JSX";
+import Header from "../components/Header.jsx";
 import Steps from "../components/Steps";
 import Description from "../components/Description";
 import Testimonials from "../components/Testimonials";
